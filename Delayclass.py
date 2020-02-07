a7ad8897652180b8b4dc00a2b9a9f05d226aa234
sclark003 <60785645+sclark003@users.noreply.github.com>
2020-02-07 14:55:35 +0000

    Update Delayclass.py

--- Delayclass.py
+++ Delayclass.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 class Delay:
+    #delay by n number of samples
     def __init__(self,n):
         self.n = n
         self.buffer = np.zeros(n)
-                
+     
+    #delay input array x by n samples
     def delay(self,x):
         l = int(len(x))
         for i in range(l):
